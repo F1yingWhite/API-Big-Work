a2930e6c71b75dd424b0ee83f06bc8ef46428b21
SuRTian <sl15611653630@163.com>
2024-05-28 17:37:58 +0800

    对history尽心修改，增加了访问时间。新增推荐算法recommandmovie

--- models/history.go
+++ models/history.go
@@ -4,14 +4,16 @@ import "time"
 
 type History struct {
 	CreatedAt *time.Time `json:"time"`
-	MovieID   uint        `json:"movie_id"`
+	MovieID   uint       `json:"movie_id"`
 	UserID    string     `json:"user_id"`
 }
 
 func CreateHistory(movieID uint, userID string) error {
+	now := time.Now()
 	history := History{
-		MovieID: movieID,
-		UserID:  userID,
+		MovieID:   movieID,
+		UserID:    userID,
+		CreatedAt: &now, //记录创建时间 like相同则推荐最新的
 	}
 	return DB.Create(&history).Error
 }
