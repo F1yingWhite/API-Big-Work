052eae166e0bc4f40cbdec67e97a6de9ea2160f0
SuRTian <sl15611653630@163.com>
2024-05-28 15:31:13 +0800

    小改

--- models/like.go
+++ models/like.go
@@ -1,7 +1,8 @@
 package models
 
+//记录用户点赞记录，已经点赞过的不能再点赞
 type Like struct {
 	ID      int    `json:"id" gorm:"primaryKey"`
-	MovieID uint    `json:"movie_id"`
+	MovieID uint   `json:"movie_id"`
 	UserID  string `json:"user_id"`
 }
